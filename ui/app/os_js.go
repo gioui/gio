90d2174090eb3608056aff9cdbd27be171c0dd90
Elias Naur <mail@eliasnaur.com>
2019-05-13 12:49:43 +0200

    ui/app: take input focus when initializing the HTML canvas element
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- ui/app/os_js.go
+++ ui/app/os_js.go
@@ -56,6 +56,7 @@ func createWindow(opts *WindowOptions) error {
 	w.w = newWindow(w)
 	go func() {
 		windows <- w.w
+		w.focus()
 		w.w.event(ChangeStage{StageRunning})
 		w.draw(true)
 		select {}
