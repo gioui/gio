6f9f07e860e02451c6e318131a4ff704e8a8c3d9
Elias Naur <mail@eliasnaur.com>
2019-06-09 15:43:09 +0200

    apps/gophers: delete support files
    Now that the gio tool can generate runnable mobile and webassembly
    apps, the support files are no longer needed.
    
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- apps/gophers/ios/gophers/main.m
+++
@@ -1,8 +0,0 @@
-@import UIKit;
-@import Gophers;
-
-int main(int argc, char * argv[]) {
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GioAppDelegate class]));
-    }
-}
