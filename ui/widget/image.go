ca24026ffd2cd5a93e8f74c659c471ebfa18d739
Elias Naur <mail@eliasnaur.com>
2019-07-09 14:39:08 +0200

    ui/widget: preserve image aspect in Image.Layout
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- ui/widget/image.go
+++ ui/widget/image.go
@@ -17,12 +17,25 @@ type Image struct {
 }
 
 func (im Image) Layout(ops *ui.Ops, cs layout.Constraints) layout.Dimens {
+	size := im.Src.Bounds()
+	w, h := size.Dx(), size.Dy()
+	if w == 0 || h == 0 {
+		return layout.Dimens{}
+	}
 	d := image.Point{X: cs.Width.Max, Y: cs.Height.Max}
 	if d.X == ui.Inf {
 		d.X = cs.Width.Min
 	}
 	if d.Y == ui.Inf {
 		d.Y = cs.Height.Min
+	}
+	aspect := float32(w) / float32(h)
+	dw, dh := float32(d.X), float32(d.Y)
+	dAspect := dw / dh
+	if aspect < dAspect {
+		d.X = int(dh*aspect + 0.5)
+	} else {
+		d.Y = int(dw/aspect + 0.5)
 	}
 	dr := f32.Rectangle{
 		Max: f32.Point{X: float32(d.X), Y: float32(d.Y)},
