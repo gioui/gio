6ce224b89fc30a1061f6aca2f6e6216fa9730e09
Elias Naur <mail@eliasnaur.com>
2019-08-02 15:48:27 +0200

    cmd/gio: don't strip debug info for webassembly builds
    The space savings are minimal and we lose function names in the
    browser debuggers.
    
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- cmd/gio/jsbuild.go
+++ cmd/gio/jsbuild.go
@@ -21,7 +21,7 @@ func buildJS(bi *buildInfo) error {
 	cmd := exec.Command(
 		"go",
 		"build",
-		"-ldflags=-w -s "+bi.ldflags,
+		"-ldflags="+bi.ldflags,
 		"-o", filepath.Join(out, "main.wasm"),
 		bi.pkg,
 	)
