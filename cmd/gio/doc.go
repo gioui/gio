3c69c81401e78ab9c8705717bb819856a26da062
Elias Naur <mail@eliasnaur.com>
2019-08-11 20:03:39 +0200

    cmd/gio: add package documentation
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

---
+++ cmd/gio/doc.go
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
+/*
+The gio tool builds and packages Gio programs for Android, iOS/tvOS
+and WebAssembly.
+
+Run gio with no arguments for instructions, or see the examples at
+https://gioui.org.
+*/
+package main
