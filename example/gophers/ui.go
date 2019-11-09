d8c9cb426c417a98e58529dfde91462ec10c5419
Elias Naur <mail@eliasnaur.com>
2019-11-09 19:45:36 +0100

    example/gophers: update gio version
    gofmt -w -s . as well.
    
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- example/gophers/ui.go
+++ example/gophers/ui.go
@@ -19,6 +19,7 @@ import (
 	"gioui.org/io/profile"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/text"
 	"gioui.org/unit"
@@ -372,26 +373,9 @@ func (c *clipCircle) Layout(gtx *layout.Context, w layout.Widget) {
 	rr := szf * .5
 	var stack op.StackOp
 	stack.Push(gtx.Ops)
-	rrect(gtx.Ops, szf, szf, rr, rr, rr, rr)
+	clip.RoundRect(gtx.Ops, f32.Rectangle{Max: f32.Point{X: szf, Y: szf}}, rr, rr, rr, rr).Add(gtx.Ops)
 	m.Add(gtx.Ops)
 	stack.Pop()
-}
-
-// https://pomax.github.io/bezierinfo/#circles_cubic.
-func rrect(ops *op.Ops, width, height, se, sw, nw, ne float32) {
-	w, h := float32(width), float32(height)
-	const c = 0.55228475 // 4*(sqrt(2)-1)/3
-	var b paint.Path
-	b.Begin(ops)
-	b.Move(f32.Point{X: w, Y: h - se})
-	b.Cube(f32.Point{X: 0, Y: se * c}, f32.Point{X: -se + se*c, Y: se}, f32.Point{X: -se, Y: se}) // SE
-	b.Line(f32.Point{X: sw - w + se, Y: 0})
-	b.Cube(f32.Point{X: -sw * c, Y: 0}, f32.Point{X: -sw, Y: -sw + sw*c}, f32.Point{X: -sw, Y: -sw}) // SW
-	b.Line(f32.Point{X: 0, Y: nw - h + sw})
-	b.Cube(f32.Point{X: 0, Y: -nw * c}, f32.Point{X: nw - nw*c, Y: -nw}, f32.Point{X: nw, Y: -nw}) // NW
-	b.Line(f32.Point{X: w - ne - nw, Y: 0})
-	b.Cube(f32.Point{X: ne * c, Y: 0}, f32.Point{X: ne, Y: ne - ne*c}, f32.Point{X: ne, Y: ne}) // NE
-	b.End().Add(ops)
 }
 
 const longTextSample = `1. I learned from my grandfather, Verus, to use good manners, and to
