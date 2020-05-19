dac8ffc00271ae07c18dcfb1c608f284d9a7f7ea
Elias Naur <mail@eliasnaur.com>
2020-05-19 10:12:11 +0200

    layout: add Exact for constructing rigid Constraints matching a size
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- layout/layout.go
+++ layout/layout.go
@@ -58,6 +58,14 @@ const (
 	Vertical
 )
 
+// Exact returns the Constraints with the minimum and maximum size
+// set to size.
+func Exact(size image.Point) Constraints {
+	return Constraints{
+		Min: size, Max: size,
+	}
+}
+
 // Constrain a size so each dimension is in the range [min;max].
 func (c Constraints) Constrain(size image.Point) image.Point {
 	if min := c.Min.X; size.X < min {
