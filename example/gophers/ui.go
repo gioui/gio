5a6ad65b853658b3ac4e13fac347aee6c847548f
Elias Naur <mail@eliasnaur.com>
2020-07-09 18:06:56 +0200

    example,cmd: bump gio version
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- example/gophers/ui.go
+++ example/gophers/ui.go
@@ -361,7 +361,7 @@ func (c *clipCircle) Layout(gtx layout.Context, w layout.Widget) layout.Dimensio
 	szf := float32(max)
 	rr := szf * .5
 	defer op.Push(gtx.Ops).Pop()
-	clip.Rect{
+	clip.RRect{
 		Rect: f32.Rectangle{Max: f32.Point{X: szf, Y: szf}},
 		NE:   rr, NW: rr, SE: rr, SW: rr,
 	}.Add(gtx.Ops)
