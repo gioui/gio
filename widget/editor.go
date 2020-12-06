gioui/gio

commit 94d242d18c9245e17384d2b30d2b4fdbde8ad5ef

op/paint: remove support for PaintOp.Rect
PaintOp.Rect is the wrong abstraction; it implies a clip operation
better handled by package clip, and not all paints need it (colors).
Furthermore, it's awkward to specify a PaintOp that fills up the
current clip area, regardless of its size.

Redefine PathOp to mean "fill current clip area".

API change. Replace uses of PaintOp.Rect with a TransformOp applied
before the PaintOp.

Leave a TODO for the PathOp infinity area.

Fixes gio#167

Signed-off-by: Elias Naur <mail@eliasnaur.com>

widget/editor.go
@@ -19,6 +19,7 @@ import (
 	"gioui.org/io/pointer"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/text"
 	"gioui.org/unit"
@@ -432,7 +433,7 @@ func (e *Editor) PaintText(gtx layout.Context) {
 		stack := op.Push(gtx.Ops)
 		op.Offset(shape.offset).Add(gtx.Ops)
 		shape.clip.Add(gtx.Ops)
-		paint.PaintOp{Rect: layout.FRect(clip).Sub(shape.offset)}.Add(gtx.Ops)
+		paint.PaintOp{}.Add(gtx.Ops)
 		stack.Pop()
 	}
 }
@@ -457,19 +458,22 @@ func (e *Editor) PaintCaret(gtx layout.Context) {
 		X: -e.scrollOff.X,
 		Y: -e.scrollOff.Y,
 	})
-	clip := textPadding(e.lines)
+	cl := textPadding(e.lines)
 	// Account for caret width to each side.
 	whalf := (carWidth / 2).Ceil()
-	if clip.Max.X < whalf {
-		clip.Max.X = whalf
+	if cl.Max.X < whalf {
+		cl.Max.X = whalf
 	}
-	if clip.Min.X > -whalf {
-		clip.Min.X = -whalf
+	if cl.Min.X > -whalf {
+		cl.Min.X = -whalf
 	}
-	clip.Max = clip.Max.Add(e.viewSize)
-	carRect = clip.Intersect(carRect)
+	cl.Max = cl.Max.Add(e.viewSize)
+	carRect = cl.Intersect(carRect)
 	if !carRect.Empty() {
-		paint.PaintOp{Rect: layout.FRect(carRect)}.Add(gtx.Ops)
+		st := op.Push(gtx.Ops)
+		clip.Rect(carRect).Add(gtx.Ops)
+		paint.PaintOp{}.Add(gtx.Ops)
+		st.Pop()
 	}
 }
 

widget/label.go
@@ -106,12 +106,11 @@ func (l Label) Layout(gtx layout.Context, s text.Shaper, font text.Font, size un
 		if !ok {
 			break
 		}
-		lclip := layout.FRect(clip).Sub(off)
 		stack := op.Push(gtx.Ops)
 		op.Offset(off).Add(gtx.Ops)
 		str := txt[start:end]
 		s.ShapeString(font, textSize, str, l).Add(gtx.Ops)
-		paint.PaintOp{Rect: lclip}.Add(gtx.Ops)
+		paint.PaintOp{}.Add(gtx.Ops)
 		stack.Pop()
 	}
 	return dims

app/headless/headless_test.go
@@ -43,14 +43,9 @@ func TestClipping(t *testing.T) {
 	w, release := newTestWindow(t)
 	defer release()
 
-	sz := w.size
 	col := color.RGBA{A: 0xff, R: 0xca, G: 0xfe}
 	col2 := color.RGBA{A: 0xff, R: 0x00, G: 0xfe}
 	var ops op.Ops
-	pop := paint.PaintOp{Rect: f32.Rectangle{Max: f32.Point{
-		X: float32(sz.X),
-		Y: float32(sz.Y),
-	}}}
 	paint.ColorOp{Color: col}.Add(&ops)
 	clip.RRect{
 		Rect: f32.Rectangle{
@@ -59,7 +54,7 @@ func TestClipping(t *testing.T) {
 		},
 		SE: 75,
 	}.Add(&ops)
-	pop.Add(&ops)
+	paint.PaintOp{}.Add(&ops)
 	paint.ColorOp{Color: col2}.Add(&ops)
 	clip.RRect{
 		Rect: f32.Rectangle{
@@ -68,7 +63,7 @@ func TestClipping(t *testing.T) {
 		},
 		NW: 75,
 	}.Add(&ops)
-	pop.Add(&ops)
+	paint.PaintOp{}.Add(&ops)
 	if err := w.Frame(&ops); err != nil {
 		t.Fatal(err)
 	}

gpu/gpu.go
@@ -25,7 +25,6 @@ import (
 	gunsafe "gioui.org/internal/unsafe"
 	"gioui.org/layout"
 	"gioui.org/op"
-	"gioui.org/op/paint"
 )
 
 type GPU struct {
@@ -213,26 +212,6 @@ func decodeLinearGradientOp(data []byte) linearGradientOpData {
 			B: data[21+2],
 			A: data[21+3],
 		},
-	}
-}
-
-func decodePaintOp(data []byte) paint.PaintOp {
-	bo := binary.LittleEndian
-	if opconst.OpType(data[0]) != opconst.TypePaint {
-		panic("invalid op")
-	}
-	r := f32.Rectangle{
-		Min: f32.Point{
-			X: math.Float32frombits(bo.Uint32(data[1:])),
-			Y: math.Float32frombits(bo.Uint32(data[5:])),
-		},
-		Max: f32.Point{
-			X: math.Float32frombits(bo.Uint32(data[9:])),
-			Y: math.Float32frombits(bo.Uint32(data[13:])),
-		},
-	}
-	return paint.PaintOp{
-		Rect: r,
 	}
 }
 
@@ -854,12 +833,18 @@ loop:
 			state.matType = materialTexture
 			state.image = decodeImageOp(encOp.Data, encOp.Refs)
 		case opconst.TypePaint:
-			op := decodePaintOp(encOp.Data)
 			// Transform (if needed) the painting rectangle and if so generate a clip path,
 			// for those cases also compute a partialTrans that maps texture coordinates between
 			// the new bounding rectangle and the transformed original paint rectangle.
 			trans, off := splitTransform(state.t)
-			clipData, bnd, partialTrans := d.boundsForTransformedRect(op.Rect, trans)
+			// Fill the clip area, unless the material is a (bounded) image.
+			// TODO: Find a tighter bound.
+			inf := float32(1e6)
+			dst := f32.Rect(-inf, -inf, inf, inf)
+			if state.matType == materialTexture {
+				dst = layout.FRect(state.image.src.Rect)
+			}
+			clipData, bnd, partialTrans := d.boundsForTransformedRect(dst, trans)
 			clip := state.clip.Intersect(bnd.Add(off))
 			if clip.Empty() {
 				continue

internal/opconst/ops.go
@@ -36,7 +36,7 @@ const (
 	TypeLayerLen          = 1
 	TypeRedrawLen         = 1 + 8
 	TypeImageLen          = 1
-	TypePaintLen          = 1 + 4*4
+	TypePaintLen          = 1
 	TypeColorLen          = 1 + 4
 	TypeLinearGradientLen = 1 + 8*2 + 4*2
 	TypeAreaLen           = 1 + 1 + 4*4

internal/rendertest/bench_test.go
@@ -171,7 +171,7 @@ func draw1000CirclesInstanced(gtx layout.Context) {
 
 	r := op.Record(ops)
 	clip.RRect{Rect: f32.Rect(0, 0, 10, 10), NE: 5, SE: 5, SW: 5, NW: 5}.Add(ops)
-	paint.PaintOp{Rect: f32.Rect(0, 0, 10, 10)}.Add(ops)
+	paint.PaintOp{}.Add(ops)
 	c := r.Stop()
 
 	for x := 0; x < 100; x++ {
@@ -228,7 +228,7 @@ func drawShapeInstances(gtx layout.Context, th *material.Theme) chan op.CallOp {
 
 		r := op.Record(ops)
 		clip.RRect{Rect: f32.Rect(0, 0, 25, 25), NE: 10, SE: 10, SW: 10, NW: 10}.Add(ops)
-		paint.PaintOp{Rect: f32.Rect(0, 0, 25, 25)}.Add(ops)
+		paint.PaintOp{}.Add(ops)
 		c := r.Stop()
 
 		squares.Add(ops)

internal/rendertest/clip_test.go
@@ -81,7 +81,8 @@ func TestPaintArc(t *testing.T) {
 func TestPaintTexture(t *testing.T) {
 	run(t, func(o *op.Ops) {
 		squares.Add(o)
-		paint.PaintOp{Rect: f32.Rect(0, 0, 80, 80)}.Add(o)
+		scale(80.0/512, 80.0/512).Add(o)
+		paint.PaintOp{}.Add(o)
 	}, func(r result) {
 		r.expect(0, 0, colornames.Blue)
 		r.expect(79, 10, colornames.Green)
@@ -94,7 +95,8 @@ func TestPaintClippedTexture(t *testing.T) {
 	run(t, func(o *op.Ops) {
 		squares.Add(o)
 		clip.RRect{Rect: f32.Rect(0, 0, 40, 40)}.Add(o)
-		paint.PaintOp{Rect: f32.Rect(0, 0, 80, 80)}.Add(o)
+		scale(80.0/512, 80.0/512).Add(o)
+		paint.PaintOp{}.Add(o)
 	}, func(r result) {
 		r.expect(40, 40, colornames.White)
 		r.expect(25, 35, colornames.Blue)

internal/rendertest/render_test.go
@@ -35,10 +35,10 @@ func TestTransformMacro(t *testing.T) {
 		stack := op.Push(o)
 		op.Offset(f32.Pt(0, 10)).Add(o)
 
-		// Actually create the text clip-path
+		// Apply the clip-path.
 		c.Add(o)
 
-		paint.PaintOp{Rect: f32.Rect(0, 0, 10, 10)}.Add(o)
+		paint.PaintOp{}.Add(o)
 		stack.Pop()
 
 		c2 := m2.Stop()
@@ -124,7 +124,7 @@ func constSqCirc() op.CallOp {
 func drawChild(ops *op.Ops, text op.CallOp) op.CallOp {
 	r1 := op.Record(ops)
 	text.Add(ops)
-	paint.PaintOp{Rect: f32.Rect(0, 0, 10, 10)}.Add(ops)
+	paint.PaintOp{}.Add(ops)
 	return r1.Stop()
 }
 
@@ -159,7 +159,7 @@ func TestBuildOffscreen(t *testing.T) {
 		s := op.Push(o)
 		op.Offset(f32.Pt(0, off)).Add(o)
 		txt.Add(o)
-		paint.PaintOp{Rect: f32.Rect(0, 0, 40, 40)}.Add(o)
+		paint.PaintOp{}.Add(o)
 		s.Pop()
 	}
 
@@ -184,7 +184,8 @@ func TestBuildOffscreen(t *testing.T) {
 func TestNegativeOverlaps(t *testing.T) {
 	run(t, func(ops *op.Ops) {
 		clip.RRect{Rect: f32.Rect(50, 50, 100, 100)}.Add(ops)
-		paint.PaintOp{Rect: f32.Rect(0, 120, 100, 122)}.Add(ops)
+		clip.Rect(image.Rect(0, 120, 100, 122)).Add(ops)
+		paint.PaintOp{}.Add(ops)
 	}, func(r result) {
 		r.expect(60, 60, colornames.White)
 		r.expect(60, 110, colornames.White)
@@ -207,6 +208,8 @@ var gradients = []Gradient{
 }
 
 func TestLinearGradient(t *testing.T) {
+	t.Skip("linear gradients don't support transformations")
+
 	const gradienth = 8
 	// 0.5 offset from ends to ensure that the center of the pixel
 	// aligns with gradient from and to colors.
@@ -214,7 +217,7 @@ func TestLinearGradient(t *testing.T) {
 	samples := []int{0, 12, 32, 64, 96, 115, 127}
 
 	run(t, func(ops *op.Ops) {
-		gr := pixelAligned
+		gr := f32.Rect(0, 0, 128, gradienth)
 		for _, g := range gradients {
 			paint.LinearGradientOp{
 				Stop1:  f32.Pt(gr.Min.X, gr.Min.Y),
@@ -222,7 +225,12 @@ func TestLinearGradient(t *testing.T) {
 				Stop2:  f32.Pt(gr.Max.X, gr.Min.Y),
 				Color2: g.To,
 			}.Add(ops)
-			paint.PaintOp{Rect: gr}.Add(ops)
+			st := op.Push(ops)
+			clip.RRect{Rect: gr}.Add(ops)
+			op.Affine(f32.Affine2D{}.Offset(pixelAligned.Min)).Add(ops)
+			scale(pixelAligned.Dx()/128, 1).Add(ops)
+			paint.PaintOp{}.Add(ops)
+			st.Pop()
 			gr = gr.Add(f32.Pt(0, gradienth))
 		}
 	}, func(r result) {
@@ -247,7 +255,10 @@ func TestLinearGradientAngled(t *testing.T) {
 			Stop2:  f32.Pt(0, 0),
 			Color2: colornames.Red,
 		}.Add(ops)
-		paint.PaintOp{Rect: f32.Rect(0, 0, 64, 64)}.Add(ops)
+		st := op.Push(ops)
+		clip.Rect(image.Rect(0, 0, 64, 64)).Add(ops)
+		paint.PaintOp{}.Add(ops)
+		st.Pop()
 
 		paint.LinearGradientOp{
 			Stop1:  f32.Pt(64, 64),
@@ -255,7 +266,10 @@ func TestLinearGradientAngled(t *testing.T) {
 			Stop2:  f32.Pt(128, 0),
 			Color2: colornames.Green,
 		}.Add(ops)
-		paint.PaintOp{Rect: f32.Rect(64, 0, 128, 64)}.Add(ops)
+		st = op.Push(ops)
+		clip.Rect(image.Rect(64, 0, 128, 64)).Add(ops)
+		paint.PaintOp{}.Add(ops)
+		st.Pop()
 
 		paint.LinearGradientOp{
 			Stop1:  f32.Pt(64, 64),
@@ -263,7 +277,10 @@ func TestLinearGradientAngled(t *testing.T) {
 			Stop2:  f32.Pt(128, 128),
 			Color2: colornames.Blue,
 		}.Add(ops)
-		paint.PaintOp{Rect: f32.Rect(64, 64, 128, 128)}.Add(ops)
+		st = op.Push(ops)
+		clip.Rect(image.Rect(64, 64, 128, 128)).Add(ops)
+		paint.PaintOp{}.Add(ops)
+		st.Pop()
 
 		paint.LinearGradientOp{
 			Stop1:  f32.Pt(64, 64),
@@ -271,7 +288,10 @@ func TestLinearGradientAngled(t *testing.T) {
 			Stop2:  f32.Pt(0, 128),
 			Color2: colornames.Magenta,
 		}.Add(ops)
-		paint.PaintOp{Rect: f32.Rect(0, 64, 64, 128)}.Add(ops)
+		st = op.Push(ops)
+		clip.Rect(image.Rect(0, 64, 64, 128)).Add(ops)
+		paint.PaintOp{}.Add(ops)
+		st.Pop()
 	}, func(r result) {})
 }
 

commit ede632b2650d2d7913883aabddf9918c7b60218b

widget: fix Editor and Label clipping
Commit gioui.org/commit/94d242d18c9245 broke Editor and Label clipping,
most visible for single-line Editors. Restore the correct clipping.

Signed-off-by: Elias Naur <mail@eliasnaur.com>

widget/editor.go
@@ -146,7 +146,7 @@ type SubmitEvent struct {
 }
 
 type line struct {
-	offset f32.Point
+	offset image.Point
 	clip   op.CallOp
 }
 
@@ -447,12 +447,13 @@ func (e *Editor) layout(gtx layout.Context) layout.Dimensions {
 }
 
 func (e *Editor) PaintText(gtx layout.Context) {
-	clip := textPadding(e.lines)
-	clip.Max = clip.Max.Add(e.viewSize)
+	cl := textPadding(e.lines)
+	cl.Max = cl.Max.Add(e.viewSize)
 	for _, shape := range e.shapes {
 		stack := op.Push(gtx.Ops)
-		op.Offset(shape.offset).Add(gtx.Ops)
+		op.Offset(layout.FPt(shape.offset)).Add(gtx.Ops)
 		shape.clip.Add(gtx.Ops)
+		clip.Rect(cl.Sub(shape.offset)).Add(gtx.Ops)
 		paint.PaintOp{}.Add(gtx.Ops)
 		stack.Pop()
 	}

widget/label.go
@@ -7,9 +7,9 @@ import (
 	"image"
 	"unicode/utf8"
 
-	"gioui.org/f32"
 	"gioui.org/layout"
 	"gioui.org/op"
+	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/text"
 	"gioui.org/unit"
@@ -38,7 +38,7 @@ type lineIterator struct {
 
 const inf = 1e6
 
-func (l *lineIterator) Next() (text.Layout, f32.Point, bool) {
+func (l *lineIterator) Next() (text.Layout, image.Point, bool) {
 	for len(l.Lines) > 0 {
 		line := l.Lines[0]
 		l.Lines = l.Lines[1:]
@@ -82,10 +82,10 @@ func (l *lineIterator) Next() (text.Layout, f32.Point, bool) {
 			endx += layout.Advances[rune]
 			rune++
 		}
-		offf := f32.Point{X: float32(off.X) / 64, Y: float32(off.Y) / 64}
+		offf := image.Point{X: off.X.Floor(), Y: off.Y.Floor()}
 		return layout, offf, true
 	}
-	return text.Layout{}, f32.Point{}, false
+	return text.Layout{}, image.Point{}, false
 }
 
 func (l Label) Layout(gtx layout.Context, s text.Shaper, font text.Font, size unit.Value, txt string) layout.Dimensions {
@@ -97,11 +97,11 @@ func (l Label) Layout(gtx layout.Context, s text.Shaper, font text.Font, size un
 	}
 	dims := linesDimens(lines)
 	dims.Size = cs.Constrain(dims.Size)
-	clip := textPadding(lines)
-	clip.Max = clip.Max.Add(dims.Size)
+	cl := textPadding(lines)
+	cl.Max = cl.Max.Add(dims.Size)
 	it := lineIterator{
 		Lines:     lines,
-		Clip:      clip,
+		Clip:      cl,
 		Alignment: l.Alignment,
 		Width:     dims.Size.X,
 	}
@@ -111,8 +111,9 @@ func (l Label) Layout(gtx layout.Context, s text.Shaper, font text.Font, size un
 			break
 		}
 		stack := op.Push(gtx.Ops)
-		op.Offset(off).Add(gtx.Ops)
+		op.Offset(layout.FPt(off)).Add(gtx.Ops)
 		s.Shape(font, textSize, l).Add(gtx.Ops)
+		clip.Rect(cl.Sub(off)).Add(gtx.Ops)
 		paint.PaintOp{}.Add(gtx.Ops)
 		stack.Pop()
 	}