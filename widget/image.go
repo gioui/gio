9793fcfcd0ac7a0d0c231d3a964d77cf7e9a9294
Egon Elbre <egonelbre@gmail.com>
2021-03-01 12:26:36 +0200

    widget: add Fit and Position to Image for image placement
    Signed-off-by: Egon Elbre <egonelbre@gmail.com>

--- widget/image.go
+++ widget/image.go
@@ -8,7 +8,6 @@ import (
 	"gioui.org/f32"
 	"gioui.org/layout"
 	"gioui.org/op"
-	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/unit"
 )
@@ -17,6 +16,12 @@ import (
 type Image struct {
 	// Src is the image to display.
 	Src paint.ImageOp
+	// Fit specifies how to scale the image to the constraints.
+	// By default it does not do any scaling.
+	Fit Fit
+	// Position specifies where to position the image within
+	// the constraints.
+	Position layout.Direction
 	// Scale is the ratio of image pixels to
 	// dps. If Scale is zero Image falls back to
 	// a scale that match a standard 72 DPI.
@@ -26,21 +31,24 @@ type Image struct {
 const defaultScale = float32(160.0 / 72.0)
 
 func (im Image) Layout(gtx layout.Context) layout.Dimensions {
+	defer op.Save(gtx.Ops).Load()
+
 	scale := im.Scale
 	if scale == 0 {
 		scale = defaultScale
 	}
+
 	size := im.Src.Size()
 	wf, hf := float32(size.X), float32(size.Y)
 	w, h := gtx.Px(unit.Dp(wf*scale)), gtx.Px(unit.Dp(hf*scale))
-	cs := gtx.Constraints
-	d := cs.Constrain(image.Pt(w, h))
-	stack := op.Save(gtx.Ops)
+
+	dims := im.Fit.scale(gtx, im.Position, layout.Dimensions{Size: image.Pt(w, h)})
+
 	pixelScale := scale * gtx.Metric.PxPerDp
 	op.Affine(f32.Affine2D{}.Scale(f32.Point{}, f32.Pt(pixelScale, pixelScale))).Add(gtx.Ops)
-	clip.Rect(image.Rectangle{Max: d}).Add(gtx.Ops)
+
 	im.Src.Add(gtx.Ops)
 	paint.PaintOp{}.Add(gtx.Ops)
-	stack.Load()
-	return layout.Dimensions{Size: d}
+
+	return dims
 }
