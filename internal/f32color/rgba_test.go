2296c80d288b4c3b811a1af3fe6c4364c99cdb00
Elias Naur <mail@eliasnaur.com>
2021-04-21 17:17:39 +0200

    internal/f32color: delete unused NRGBAToRGBA_PostAlpha
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- internal/f32color/rgba_test.go
+++ internal/f32color/rgba_test.go
@@ -7,21 +7,6 @@ import (
 	"testing"
 )
 
-func TestNRGBAToRGBA_PostAlpha_Boundary(t *testing.T) {
-	for col := 0; col <= 0xFF; col++ {
-		for alpha := 0; alpha <= 0xFF; alpha++ {
-			in := color.NRGBA{R: uint8(col), A: uint8(alpha)}
-			premul := NRGBAToRGBA_PostAlpha(in)
-			if premul.A != uint8(alpha) {
-				t.Errorf("%v: got %v expected %v", in, premul.A, alpha)
-			}
-			if premul.R > premul.A {
-				t.Errorf("%v: R=%v > A=%v", in, premul.R, premul.A)
-			}
-		}
-	}
-}
-
 func TestNRGBAToLinearRGBA_Boundary(t *testing.T) {
 	for col := 0; col <= 0xFF; col++ {
 		for alpha := 0; alpha <= 0xFF; alpha++ {
