6ede60d84eaa41f12233e940759c6a6114cfc338
Elias Naur <mail@eliasnaur.com>
2022-04-24 10:50:10 +0200

    app: [Android] avoid out-of-bounds access in getCursorCapsMode
    Fixes: https://todo.sr.ht/~eliasnaur/gio/404
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- app/GioView.java
+++ app/GioView.java
@@ -555,7 +555,11 @@ public final class GioView extends SurfaceView implements Choreographer.FrameCal
 		@Override public int getCursorCapsMode(int reqModes) {
 			Snippet snip = getSnippet();
 			int selStart = imeSelectionStart(nhandle);
-			return TextUtils.getCapsMode(snip.snippet, imeToUTF16(nhandle, selStart - snip.offset), reqModes);
+			int off = imeToUTF16(nhandle, selStart - snip.offset);
+			if (off < 0 || off > snip.snippet.length()) {
+				return 0;
+			}
+			return TextUtils.getCapsMode(snip.snippet, off, reqModes);
 		}
 
 		@Override public ExtractedText getExtractedText(ExtractedTextRequest request, int flags) {
