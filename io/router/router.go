28c206fc78c76b1481fc3ed4c28ce3562ce424ba
Elias Naur <mail@eliasnaur.com>
2022-07-20 10:37:28 +0200

    io/router: try all handlers if a key don't match the focus ancestor tree
    When a key.InputOp is focused, a key.Event is matched to it and its ancestors.
    If there is no focus, every handler is matched.
    This change always matches to every handler, after checking the focus and
    its ancestors.
    
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- io/router/router.go
+++ io/router/router.go
@@ -202,6 +202,12 @@ func (q *Router) queueKeyEvent(e key.Event) {
 		n := &pq.hitTree[idx]
 		if focused {
 			idx = n.next
+			if idx == -1 {
+				// No handler found in focus ancestor tree.
+				// Try all handlers.
+				idx = len(pq.hitTree) - 1
+				focused = false
+			}
 		} else {
 			idx--
 		}
