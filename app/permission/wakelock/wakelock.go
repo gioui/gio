dbbae0519e248dbfcf8f633b196c66018b15aad9
Elias Naur <mail@eliasnaur.com>
2022-08-01 20:15:44 +0200

    app/permission/wakelock: add package for requesting wake locks
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

---
+++ app/permission/wakelock/wakelock.go
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
+/*
+Package wakelock implements permission to acquire locks that keep the system
+from suspending.
+
+Android
+
+The following entries will be added to AndroidManifest.xml:
+
+    <uses-permission android:name="android.permission.WAKE_LOCK"/>
+
+*/
+package wakelock
