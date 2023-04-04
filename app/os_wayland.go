gioui/gio

commit 2957d007a25ff4ec8c6858a056cfd40275393695

app: [Wayland] only start resize gesture on pointer down
Signed-off-by: Elias Naur <mail@eliasnaur.com>

app/os_wayland.go
@@ -886,18 +886,6 @@ func gio_onPointerButton(data unsafe.Pointer, p *C.struct_wl_pointer, serial, t,
 	switch wbtn {
 	case BTN_LEFT:
 		btn = pointer.ButtonPrimary
-		if _, edge := w.systemGesture(); edge != 0 {
-			w.resize(serial, edge)
-			return
-		}
-		act, ok := w.w.ActionAt(w.lastPos)
-		if ok && w.config.Mode == Windowed {
-			switch act {
-			case system.ActionMove:
-				w.move(serial)
-				return
-			}
-		}
 	case BTN_RIGHT:
 		btn = pointer.ButtonSecondary
 	case BTN_MIDDLE:
@@ -915,6 +903,20 @@ func gio_onPointerButton(data unsafe.Pointer, p *C.struct_wl_pointer, serial, t,
 	case 1:
 		w.pointerBtns |= btn
 		typ = pointer.Press
+	}
+	if typ == pointer.Press && btn == pointer.ButtonPrimary {
+		if _, edge := w.systemGesture(); edge != 0 {
+			w.resize(serial, edge)
+			return
+		}
+		act, ok := w.w.ActionAt(w.lastPos)
+		if ok && w.config.Mode == Windowed {
+			switch act {
+			case system.ActionMove:
+				w.move(serial)
+				return
+			}
+		}
 	}
 	w.flushScroll()
 	w.resetFling()

commit ad3db5212d10059bfe8af3ed75b60711d5705955

app: [Wayland] avoid stuck primary button when invoking window management
Clicking on the window border or the title bar initiates resizing and
moving of the window respectively. This commit fixes a bug where this
would cause a stuck pressed primary button, as we won't receive a
release event. The fix is to only update the set of pressed buttons
after we've decided not to invoke window management.

This fixes a regression introduced by
2957d007a25ff4ec8c6858a056cfd40275393695.

Signed-off-by: Dominik Honnef <dominik@honnef.co>

app/os_wayland.go
@@ -903,18 +903,7 @@ func gio_onPointerButton(data unsafe.Pointer, p *C.struct_wl_pointer, serial, t,
 	default:
 		return
 	}
-	var typ pointer.Type
-	switch state {
-	case 0:
-		w.pointerBtns &^= btn
-		typ = pointer.Release
-		// Move or resize gestures no longer applies.
-		w.inCompositor = false
-	case 1:
-		w.pointerBtns |= btn
-		typ = pointer.Press
-	}
-	if typ == pointer.Press && btn == pointer.ButtonPrimary {
+	if state == 1 && btn == pointer.ButtonPrimary {
 		if _, edge := w.systemGesture(); edge != 0 {
 			w.resize(serial, edge)
 			return
@@ -927,6 +916,17 @@ func gio_onPointerButton(data unsafe.Pointer, p *C.struct_wl_pointer, serial, t,
 				return
 			}
 		}
+	}
+	var typ pointer.Type
+	switch state {
+	case 0:
+		w.pointerBtns &^= btn
+		typ = pointer.Release
+		// Move or resize gestures no longer applies.
+		w.inCompositor = false
+	case 1:
+		w.pointerBtns |= btn
+		typ = pointer.Press
 	}
 	w.flushScroll()
 	w.resetFling()