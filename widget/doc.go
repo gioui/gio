5a843bee615a5f7b9fcc0660cbbe1aa9eca5c087
Elias Naur <mail@eliasnaur.com>
2023-11-28 16:00:32 -0600

    widget: update documentation
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- widget/doc.go
+++ widget/doc.go
@@ -2,5 +2,5 @@
 
 // Package widget implements state tracking and event handling of
 // common user interface controls. To draw widgets, use a theme
-// packages such as package gioui.org/widget/material.
+// packages such as package [gioui.org/widget/material].
 package widget
