8b0f062131580f4a84db99130af94c2fd5cc65c5
inkeliz <inkeliz@inkeliz.com>
2024-06-10 20:49:57 +0100

    app,io,widget: [android/macos] fix focus issues (#3)
    This patch is intended to fix two issues related to focus and
    software-keyboard.
    
    The focus is now reseted if it's lost due to external event,
    such as clicking on non-Gio window/activity/fragment.
    
    The software-keyboard will now re-open when clicked, except
    if it's ReadOnly.
    
    On macOS it will request focus again if the focus is lose
    due to another View in the same Window. However, currently,
    it's done by using ShowTextInput.
    
    Fixes: https://todo.sr.ht/~eliasnaur/gio/591
    
    Signed-off-by: inkeliz <inkeliz@inkeliz.com>

--- app/os_macos.go
+++ app/os_macos.go
@@ -298,6 +298,11 @@ static void invalidateCharacterCoordinates(CFTypeRef viewRef) {
 		}
 	}
 }
+
+static void setFocus(CFTypeRef viewRef) {
+     NSView *view = (__bridge NSView *)viewRef;
+     [view.window makeFirstResponder:view];
+}
 */
 import "C"
 
@@ -523,7 +528,11 @@ func (w *window) EditorStateChanged(old, new editorState) {
 	}
 }
 
-func (w *window) ShowTextInput(show bool) {}
+func (w *window) ShowTextInput(show bool) {
+	if show && !w.config.Focused {
+		C.setFocus(w.view)
+	}
+}
 
 func (w *window) SetInputHint(_ key.InputHint) {}
 
