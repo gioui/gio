e38f80adc671782ee0cdc002986e2f4ed616a9d6
CoyAce <akeycoy@gmail.com>
2026-01-07 19:17:02 +0800

    app/permission: add microphone permissions
    Microphone permissions enables the Android permissions RECORD_AUDIO
    
    Signed-off-by: CoyAce <AkeyCoy@gmail.com>

---
+++ app/permission/microphone/main.go
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Unlicense OR MIT
+
+/*
+Package microphone implements permissions to access microphone hardware.
+
+# Android
+
+The following entries will be added to AndroidManifest.xml:
+
+	<uses-permission android:name="android.permission.RECORD_AUDIO"/>
+
+RECORD_AUDIO is a "dangerous" permission. See documentation for package
+gioui.org/app/permission for more information.
+*/
+package microphone
