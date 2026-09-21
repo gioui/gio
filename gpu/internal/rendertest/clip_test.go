gioui/gio

commit 773a7e48a0f223358f818e7ef63da6cbad6403eb

op/clip: don't panic when stroking empty path
Fixes: https://todo.sr.ht/~eliasnaur/gio/367
Signed-off-by: Elias Naur <mail@eliasnaur.com>

op/clip/clip.go
@@ -49,18 +49,22 @@ func (p Op) add(o *op.Ops) {
 	path := p.path
 
 	if !path.hasSegments && p.width > 0 {
-		if p.path.shape != ops.Rect {
-			panic("only rects have empty paths")
+		switch p.path.shape {
+		case ops.Rect:
+			b := frect(path.bounds)
+			var rect Path
+			rect.Begin(o)
+			rect.MoveTo(b.Min)
+			rect.LineTo(f32.Pt(b.Max.X, b.Min.Y))
+			rect.LineTo(b.Max)
+			rect.LineTo(f32.Pt(b.Min.X, b.Max.Y))
+			rect.Close()
+			path = rect.End()
+		case ops.Path:
+			// Nothing to do.
+		default:
+			panic("invalid empty path for shape")
 		}
-		b := frect(path.bounds)
-		var rect Path
-		rect.Begin(o)
-		rect.MoveTo(b.Min)
-		rect.LineTo(f32.Pt(b.Max.X, b.Min.Y))
-		rect.LineTo(b.Max)
-		rect.LineTo(f32.Pt(b.Min.X, b.Max.Y))
-		rect.Close()
-		path = rect.End()
 	}
 	bo := binary.LittleEndian
 	if path.hasSegments {

op/clip/clip_test.go
@@ -62,6 +62,16 @@ func TestTransformChecks(t *testing.T) {
 	st.Pop()
 }
 
+func TestEmptyPath(t *testing.T) {
+	var ops op.Ops
+	p := clip.Path{}
+	p.Begin(&ops)
+	defer clip.Stroke{
+		Path:  p.End(),
+		Width: 3,
+	}.Op().Push(&ops).Pop()
+}
+
 func newWindow(t testing.TB, width, height int) *headless.Window {
 	w, err := headless.NewWindow(width, height)
 	if err != nil {

commit dc7e6295c1da6a8c890727b164f86393d1a702dd

op/clip: do not stroke empty paths
A Stroke clip for an empty path, or one that contains only Move and
MoveTo commands should not draw anything. Before this change, a square
the size of the Stroke.Width would be centered on its origin.

Fixes: 773a7e48a0f2 ("op/clip: don't panic when stroking empty path")
Signed-off-by: Walter Werner SCHNEIDER <contact@schnwalter.eu>
Signed-off-by: Elias Naur <mail@eliasnaur.com>

op/clip/clip.go
@@ -49,7 +49,8 @@ func (p Op) Push(o *op.Ops) Stack {
 func (p Op) add(o *op.Ops) {
 	path := p.path
 
-	if !path.hasSegments && p.width > 0 {
+	hasSegments := path.hasSegments
+	if !hasSegments && p.width > 0 {
 		switch p.path.shape {
 		case ops.Rect:
 			b := f32internal.FRect(path.bounds)
@@ -61,6 +62,7 @@ func (p Op) add(o *op.Ops) {
 			rect.LineTo(f32.Pt(b.Min.X, b.Max.Y))
 			rect.Close()
 			path = rect.End()
+			hasSegments = true
 		case ops.Path:
 			// Nothing to do.
 		default:
@@ -68,7 +70,7 @@ func (p Op) add(o *op.Ops) {
 		}
 	}
 	bo := binary.LittleEndian
-	if path.hasSegments {
+	if hasSegments {
 		data := ops.Write(&o.Internal, ops.TypePathLen)
 		data[0] = byte(ops.TypePath)
 		bo.PutUint64(data[1:], path.hash)
@@ -76,7 +78,7 @@ func (p Op) add(o *op.Ops) {
 	}
 
 	bounds := path.bounds
-	if p.width > 0 {
+	if hasSegments && p.width > 0 {
 		// Expand bounds to cover stroke.
 		half := int(p.width*.5 + .5)
 		bounds.Min.X -= half

gpu/internal/rendertest/clip_test.go
@@ -251,6 +251,41 @@ func TestStrokedPathBalloon(t *testing.T) {
 	})
 }
 
+func TestPathWithoutCommands(t *testing.T) {
+	run(t, func(o *op.Ops) {
+		var path clip.Path
+		path.Begin(o)
+		path.Close()
+		spec := path.End()
+
+		stroke := clip.Stroke{Path: spec, Width: 16}.Op().Push(o)
+		paint.Fill(o, color.NRGBA{B: 0xFF, A: 0xFF})
+		stroke.Pop()
+	}, func(r result) {
+		r.expect(0, 0, transparent)
+		r.expect(4, 4, transparent)
+	})
+}
+
+func TestPathWithoutSegments(t *testing.T) {
+	run(t, func(o *op.Ops) {
+		var path clip.Path
+		path.Begin(o)
+		path.MoveTo(f32.Pt(32, 32))
+		path.Close()
+		spec := path.End()
+
+		stroke := clip.Stroke{Path: spec, Width: 16}.Op().Push(o)
+		paint.Fill(o, color.NRGBA{B: 0xFF, A: 0xFF})
+		stroke.Pop()
+	}, func(r result) {
+		r.expect(0, 0, transparent)
+		r.expect(4, 4, transparent)
+		r.expect(28, 28, transparent)
+		r.expect(36, 36, transparent)
+	})
+}
+
 func TestPathReuse(t *testing.T) {
 	run(t, func(o *op.Ops) {
 		var path clip.Path